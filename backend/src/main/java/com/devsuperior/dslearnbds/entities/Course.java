9e925b96276d431294fe9639e456d1ab7ca6cc96
PatrickCaproni <patrick.-m@hotmail.com>
2020-11-03 15:11:33 -0300

    Resource

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -19,6 +19,7 @@ public class Course implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/enums/ResourceType.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslearnbds.enums;
+
+public enum ResourceType {
+	
+	LESSON_ONLY, LESSON,TASK, FORUM, EXTERNAL_LINK;
+
+}
