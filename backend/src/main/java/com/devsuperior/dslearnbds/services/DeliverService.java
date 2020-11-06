05621acb80f734f2daf378d0d8495faa72818757
PatrickCaproni <patrick.-m@hotmail.com>
2020-11-06 11:48:06 -0300

    Observer design pattern, OCP SOLID principle

--- backend/src/main/java/com/devsuperior/dslearnbds/services/DeliverService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/DeliverService.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.devsuperior.dslearnbds.dto.DeliverRevisionDTO;
 import com.devsuperior.dslearnbds.entities.Deliver;
+import com.devsuperior.dslearnbds.observers.DeliverRevisionObserver;
 import com.devsuperior.dslearnbds.repositories.DeliverRepository;
 
 @Service
@@ -15,6 +19,8 @@ public class DeliverService {
 	@Autowired
 	private DeliverRepository repository;
 	
+	private Set<DeliverRevisionObserver> deliverRevisionObserver = new LinkedHashSet<>(); 
+	
 	@Transactional
 	public void saveRevision(Long id, DeliverRevisionDTO dto) {
 		Deliver deliver = repository.getOne(id);
@@ -22,6 +28,14 @@ public class DeliverService {
 		deliver.setFeedback(dto.getFeedback());
 		deliver.setCorrectCount(dto.getCorrectCount());
 		repository.save(deliver);
+		
+		for(DeliverRevisionObserver observer : deliverRevisionObserver) {
+			observer.onSaveRevision(deliver);
+		}
+	}
+	
+	public void subscribeDeliverRevisionObserver(DeliverRevisionObserver observer) {
+		deliverRevisionObserver.add(observer);
 	}
 
 }
